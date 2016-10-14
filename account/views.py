985383208924eca4cd621fc51ed8e2ceaf65472b
stealstick <stillstick40@gmail.com>
2016-10-14 23:03:58 +0900

    django 리팩토링

---
+++ account/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'account/login.html')
+
+def signup(request):
+    return render(request, 'account/signup.html')
