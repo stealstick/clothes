ae79fcd83df842b3f71f9fd74833da0dac8e06ad
stealstick <stillstick40@gmail.com>
2016-10-18 10:52:37 +0900

    template, static locate

--- account/models.py
+++ account/models.py
@@ -1,12 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from django.db import models
 
 class Account(models.Model):
-    name = models.CharField(max_length=10)
-    userid = models.CharField(max_length=30)
-    userpw = models.CharField(max_length=30)
-    email = models.IntegerField(default=1)
+    user = models.OneToOneField(User)
     area_1 = models.CharField(max_length=15)
     area_2 = models.CharField(max_length=15)
     
