e4630a59aa61d83cb68cbae8c402a90ea505e46d
stealstick <stillstick40@gmail.com>
2016-10-29 10:20:06 +0900

    로그인 안한 사용자 예외 처

--- main/views.py
+++ main/views.py
@@ -2,18 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cloth
 def index(request):
+    try :
+        rent = Cloth.objects.filter(lenter = request.user)        
+    except:
+        rent = None;
     clothes = Cloth.objects.all()
-    rent = Cloth.objects.filter(lenter = request.user)
+
     context = {'clothes': clothes,
     'rent' : rent}
     return render(request, 'main/main.html', context)
 
 def category(request, category):
     category_item= Cloth.objects.filter(category=category)
-    rent = Cloth.objects.filter(lenter = request.user)
+
+    try :
+        rent = Cloth.objects.filter(lenter = request.user)        
+    except:
+        rent = None;
     context = {'category_item': category_item,
     'rent' : rent,
     'category' : category}
+
     return render(request, 'main/category.html', context)
 
 def send(request, username):
