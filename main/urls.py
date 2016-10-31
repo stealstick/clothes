952ca11a9220075536a70b3d4ca155d9191e8399
stealstick <stillstick40@gmail.com>
2016-10-31 20:07:14 +0900

    url 매

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^send/(?P<username>.+)$', views.send),
+    url(r'^borrow/$', views.borrow),
+    url(r'^sending/(?P<username>.+)$', views.sending),
     url(r'^category/(?P<category>.+)$', views.category),
+    url(r'^delete/$', views.delete),
+    url(r'^search$', views.search),
 ]
