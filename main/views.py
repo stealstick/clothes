f664e43a46e32db842fa16e554a59a4a4d934d59
stealstick <stillstick40@gmail.com>
2016-10-31 20:08:18 +0900

    쪽지 데이터 전

--- main/views.py
+++ main/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Cloth
+from mypage.models import Send
+from account.models import User
+import datetime
 def index(request):
     try :
-        rent = Cloth.objects.filter(lenter = request.user)        
+        rent = Cloth.objects.filter(lenter = request.user)
+        #send = Send.objects.filter(resiver = resiver.user)       
     except:
         rent = None;
+        #send = None;
+
+    send = Send.objects.filter(resiver = request.user)
     clothes = Cloth.objects.all()
 
     context = {'clothes': clothes,
-    'rent' : rent}
+    'rent' : rent,
+    'my_send' : send,
+    }
     return render(request, 'main/main.html', context)
 
 def category(request, category):
@@ -26,5 +35,53 @@ def category(request, category):
     return render(request, 'main/category.html', context)
 
 def send(request, username):
-    context = {'username' : username}
+    cloth_name = request.GET['item']
+    context = {'username' : username,
+    'cloth_name':cloth_name
+    }
+
     return render(request, 'main/send.html', context)
+
+def sending(request, username):
+    cloth_name = request.GET['item']
+    text = request.POST['text']
+    cloth= Cloth.objects.get(cloth_name=cloth_name)
+    resiver = User.objects.get(username=username)
+    date=datetime.datetime.now()
+    send = Send(cloth=cloth, resiver=resiver,owner=request.user, text=text, update_date=date)
+    send.save()
+    
+    return render(request, 'main/main.html')
+
+def borrow(request):
+    owner = request.GET['owner']
+    cloth = request.GET['cloth']
+    lenter = request.GET['lenter']
+    owner = User.objects.get(username=owner)
+    cloth = Cloth.objects.get(cloth_name=cloth, owner=owner)
+    lenter = User.objects.get(username=lenter)
+    cloth.lenter = lenter
+    cloth.save()
+    send=Send.objects.filter(cloth=cloth, resiver=owner, owner=lenter)
+    send.delete()
+    return HttpResponseRedirect("/mypage/")
+
+def delete(request):
+    cloth_name= request.GET['cloth_name']
+    delete_item= Cloth.objects.filter(owner =request.user, cloth_name=cloth_name)
+    delete_item.delete()
+    return HttpResponseRedirect("/mypage/")
+
+def search(request):
+    search = request.GET['search']
+    search_item=search
+    try :
+        rent = Cloth.objects.filter(lenter = request.user)        
+    except:
+        rent = None;
+    search = Cloth.objects.filter(cloth_name__contains=search)      
+    context = {'search': search,
+    'rent' : rent,
+    'search_item' : search_item,
+    }
+    return render(request, 'main/search.html', context)
