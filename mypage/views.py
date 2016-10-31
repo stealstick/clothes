83bcf402562715b8c7ed3776605225e8357301a2
stealstick <stillstick40@gmail.com>
2016-10-31 20:08:40 +0900

    메이페이지 데이터 처리

--- mypage/views.py
+++ mypage/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from main.models import Cloth
+from .models import Send
 import datetime
 
 def index(request):
     my_cloth= Cloth.objects.filter(owner=request.user)
     rent = Cloth.objects.filter(lenter = request.user)
+    send = Send.objects.filter(resiver = request.user)
     context = {'my_cloth': my_cloth,
-    'rent' : rent}
+    'rent' : rent,
+    'my_send' : send,
+    }
     return render(request, 'mypage/mypage.html', context)
